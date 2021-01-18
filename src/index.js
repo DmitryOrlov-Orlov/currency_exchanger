bb9681ab7411ac26d2f13eb79dab5891ee77f1fc
DmitryOrlov-Orlov <58471846+DmitryOrlov-Orlov@users.noreply.github.com>
2021-01-18 13:20:04 +0500

    добавил папки store, reducers, constants, action

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import './index.css';
-import App from './App';
+import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
-
-
-const store = createStore('reducer', window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+import { store } from './store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider>
-      <App store={store} />
+    <Provider store={store} >
+      <App />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
